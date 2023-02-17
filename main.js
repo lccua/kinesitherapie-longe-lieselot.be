ce167a599d0a6007d26ca0d156c76cea6af7f592
lccua <111664939+lccua@users.noreply.github.com>
2023-02-17 14:23:00 +0100

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,13 @@
+let toTop = document.querySelector(".toTop");
+window.addEventListener("scroll",function() {
+    if (window.pageYOffset > 100) {
+        toTop.classList.add("active_toTop");
+    } else {
+        toTop.classList.remove("active_toTop");
+   }
+});
+
 $(document).ready(function(){
 	$('.header').height($(window).height());
 })
+
