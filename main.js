17ba5cbb12f6b9aaa250ed22cbf27bbf0981bf02
lccua <111664939+lccua@users.noreply.github.com>
2023-04-13 13:08:00 +0200

    Update main.js

--- main.js
+++ main.js
@@ -7,6 +7,14 @@ window.addEventListener("scroll",function() {
    }
 });
 
+const vakantieBanner = document.getElementById("vakantieBanner");
+  const currentDate = new Date();
+  const endDate = new Date("2023-04-24");
+
+  if (currentDate > endDate) {
+    vakantieBanner.style.display = "none";
+  }
+
 $(document).ready(function(){
 	$('.header').height($(window).height());
 })
