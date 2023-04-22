0fd7559a64b9b3805eb9173ae43913165aedfb6f
lccua <111664939+lccua@users.noreply.github.com>
2023-04-22 16:18:14 +0200

    Add files via upload

--- main.js
+++ main.js
@@ -1,21 +1,32 @@
-let toTop = document.querySelector(".toTop");
-window.addEventListener("scroll",function() {
-    if (window.pageYOffset > 100) {
-        toTop.classList.add("active_toTop");
-    } else {
-        toTop.classList.remove("active_toTop");
-   }
-});
+const setup = () => {
+  headerFunction();
+  window.addEventListener('scroll', handleScroll);
+  hideBannerIfNeeded();
+};
 
-const vakantieBanner = document.getElementById("vakantieBanner");
+const hideBannerIfNeeded = () => {
+  const vakantieBanner = document.getElementById("vakantieBanner");
   const currentDate = new Date();
   const endDate = new Date("2023-04-24");
-
   if (currentDate > endDate) {
     vakantieBanner.style.display = "none";
   }
+}
 
-$(document).ready(function(){
-	$('.header').height($(window).height());
-})
+const headerFunction = () => {
+  $(document).ready(function(){
+    $('.header').height($(window).height());
+  });
+}
+
+const handleScroll = () => {
+  let toTop = document.querySelector(".toTop");
+  if (window.pageYOffset > 100) {
+    toTop.classList.add("active_toTop");
+  } else {
+    toTop.classList.remove("active_toTop");
+  }
+};
+
+window.addEventListener('load', setup);
 
